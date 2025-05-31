1273a2f6dbffa744c197c81b5e4fd9688d4a4ece
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-05-31 22:24:04 +0200

    Add integrations

---
+++ src/lib/data-access/installation.ts
@@ -0,0 +1,74 @@
+import "server-only";
+
+import { prisma } from "../prisma";
+
+export const getUserSolarInstallations = ({ userId }: { userId: string }) => {
+	return prisma.solarInstallation.findMany({
+		where: {
+			userId,
+		},
+	});
+};
+
+export const getUserSolarInstallationById = ({
+	userId,
+	installationId,
+}: {
+	userId: string;
+	installationId: string;
+}) => {
+	return prisma.solarInstallation.findFirst({
+		where: {
+			id: installationId,
+			userId,
+		},
+		select: {
+			id: true,
+			name: true,
+			totalPowerKw: true,
+			panelCount: true,
+			createdAt: true,
+			updatedAt: true,
+			production: {
+				// current day
+				where: {
+					createdAt: {
+						gte: new Date(new Date().setHours(0, 0, 0, 0)),
+						lte: new Date(new Date().setHours(23, 59, 59, 999)),
+					},
+				},
+			},
+		},
+	});
+};
+
+export const getSolarInstallationLastWeekProduction = ({
+	installationId,
+	userId,
+}: {
+	installationId: string;
+	userId: string;
+}) => {
+	return prisma.solarInstallationProduction.findMany({
+		where: {
+			installation: {
+				id: installationId,
+				userId: userId,
+			},
+			// last 7 days
+			createdAt: {
+				gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+			},
+		},
+		orderBy: {
+			createdAt: "asc",
+		},
+		select: {
+			id: true,
+			hourIndex: true,
+			producedEnergyKWh: true,
+			createdAt: true,
+			updatedAt: true,
+		},
+	});
+};
