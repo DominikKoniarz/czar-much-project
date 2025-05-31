dfe61b8e34657bfa68989d303e0232946a07ddf0
JanGospodarek-SC <jan.gospodarek@skycell.ch>
2025-05-31 20:23:36 +0200

    update logo

--- src/components/pages/dashboard/dashboard-header/DashboardHeader.tsx
+++ src/components/pages/dashboard/dashboard-header/DashboardHeader.tsx
@@ -10,19 +10,21 @@ const DashboardHeader = () => {
     const pathname = usePathname();
     return (
         <header className='border-b-1 flex gap-2 justify-between p-5 w-full items-center flex-wrap'>
-
-            <div className='flex gap-8 h-8'>
-                <Image src='/logo.jpg' width={80} height={200} alt='ddd'/>
-                {Object.entries(headerUrls).map(([url, name]) =>
-                    <Link
-                        href={url}
-                        key={url}
-                        className={`text-xl ${pathname.includes(url) ? "border-b-2 border-primary font-bold" : ""}`}
-                    >
-                        {name}
-                    </Link>
-                )}
+            <div className='flex gap-8 items-center'>
+                <Image src='/logo.svg' width={80} height={80} alt='ddd'/>
+                <div className='flex gap-8 h-8'>
+                    {Object.entries(headerUrls).map(([url, name]) =>
+                        <Link
+                            href={url}
+                            key={url}
+                            className={`text-xl ${pathname.includes(url) ? "border-b-2 border-primary font-bold" : ""}`}
+                        >
+                            {name}
+                        </Link>
+                    )}
+                </div>
             </div>
+
             <div className='flex gap-2'>
                 <SavingsCounter type='saved'/>
                 <SavingsCounter type='spent'/>
